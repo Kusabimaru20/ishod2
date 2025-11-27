e4dededb45c1b96fbc9d747ba7dbd8d182b5c21f
Kusabimaru20 <saiyangod1911@gmail.com>
2025-11-27 13:23:55 +0100

    ishod2

---
+++ frontend/quasar-project/src/router/routes.js
@@ -0,0 +1,17 @@
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+  
+    children: [{ path: 'UnosPage', component: () => import('pages/UnosPage.vue') }]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+]
+
+export default routes
